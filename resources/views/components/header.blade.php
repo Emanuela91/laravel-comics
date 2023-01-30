74dc0db14a7a5b21502dc81c8f1828a778e87eb3
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-30 22:17:16 +0100

    aggiunta parte di header e aggiunto css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,16 @@
+<div class="bg-primary">
+    <div class="container d-flex justify-content-end gap-5 text-white">
+        <span>DC POWER VISA</span>
+        <span>ADDITIONAL DC SITES</span>        
+    </div>
+</div>
 <div class="container-fluid">
-    <div class="container d-flex">
+    <div class="container d-flex justify-content-between mt-2 mb-2">
         <img src="{{Vite::asset('resources/img/dc-logo.png')}} " alt="" srcset="">
         <nav id="navbar" class="align-self-center">
-            <ul class="d-flex text-align-center gap-2 text-secondary">
+            <ul class="d-flex text-align-center gap-3 text-secondary fw-bold">
                 <li>CHARACTERS</li>
-                <li>COMICS</li>
+                <li class="selected">COMICS</li>
                 <li>MOVIES</li>
                 <li>TV</li>
                 <li>GAMES</li>
@@ -16,7 +22,7 @@
             </ul>
         </nav>
 
-        <form class="d-flex">
+        <form id="search" class="d-flex">
             <input type="text" class="form-control" placeholder="Search">
             <button type="button">Button</button>
         </form>
