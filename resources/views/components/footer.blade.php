718fd83b75aa0536e29c3d8c9d0252bf604bacdf
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-30 22:44:23 +0100

    aggiunta parte in blu del footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,35 @@
+<div class="bg-primary d-flex align-items-center">
+    <div class="container d-flex justify-content-between gap-5 text-white">
+        <span>DIGITAL COMICS</span>
+        <span>DC MERCHANDISE</span>      
+        <span>SUBSCRIPTION</span>  
+        <span>COMIC SHOP LOCATOR</span>
+        <span>DC POWER VISA</span>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container d-flex justify-content-between mt-2 mb-2">
+        <img src="{{Vite::asset('resources/img/dc-logo.png')}} " alt="" srcset="">
+        <nav id="navbar" class="align-self-center">
+            <ul class="d-flex text-align-center gap-3 text-secondary fw-bold">
+                <li>CHARACTERS</li>
+                <li class="selected">COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOPS</li>
+            </ul>
+        </nav>
+
+        <form id="search" class="d-flex">
+            <input type="text" class="form-control" placeholder="Search">
+            <button type="button">Button</button>
+        </form>
+
+    </div>
+
+</div>
--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -5,13 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/js/app.js')
-    {{-- segnaposto titolo in home --}}
+    {{-- segnaposto titolo in comics --}}
     @yield('head')
 </head>
 <body>
-
+    {{-- segnaposto contenuto in header --}}
     @include('components.header')
-    {{-- segnaposto contenuto in home --}}
+    {{-- segnaposto contenuto in comics --}}
     @yield('content')
+    {{-- segnaposto contenuto in footer --}}
+    @include('components.footer')
 </body>
 </html>
